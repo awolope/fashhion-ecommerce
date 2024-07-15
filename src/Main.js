38fd468a3dae192bce2f1a5379e3cecc51ffc964
Hernnie <marvelouspeculiar@gmail.com>
2024-07-15 01:16:46 +0100

    done for today

--- src/Main.js
+++ src/Main.js
@@ -13,11 +13,11 @@ const Main = () => {
       <div className="row m-5">
         <div className="col-md-6">
           <div className="m-5 h2div">
-            <h2 className="mainh2">
+            <h2 className="mainh2 ">
               <span className="focus">Fresh Looks</span> For <br />
               The Modern Classic
             </h2>
-            <p className="mainp">
+            <p className="mainp ">
               Discover the perfect fusion of classic charm and modern trends.
               Our new collection offers fresh looks that embody sophistication
               and style, ideal for the discerning fashionista who values both
@@ -29,6 +29,49 @@ const Main = () => {
           <img src="./woman.png" className="img-fluid text-center"></img>
         </div>
       </div>
+
+      <div className="row m-5">
+        <div className=" col-md-6">
+          <img src="./man.png" className="img-fluid text-center"></img>
+        </div>
+
+        <div className="col-md-6 ">
+          <div className="m-5 h2div">
+            <h2 className="mainh2 ">
+              <span className="focus">Stand Out</span> In Style
+            </h2>
+            <p className="mainp ">
+              Make a statement with our bold and stylish collection. Designed
+              for those who dare to be different, our pieces blend contemporary
+              trends with unique designs.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h3 className="text-center">
+        {" "}
+        Our <span className="focus">Shop</span>
+      </h3>
+      <div className="row">
+        <div className="col-md-4">
+          <img src="./shirt.png" className="img-fluid" alt=""></img>
+        </div>
+        <div className="col-md-4">
+          <img src="./suit.png" className="img-fluid" alt=""></img>
+        </div>
+        <div className="col-md-4">
+          <img src="./shirt.png" className="img-fluid" alt=""></img>
+        </div>
+      </div>
+      <br />
+      <Link to="/products">
+        <button className="shopbt">Shop Now</button>
+      </Link>
+      <p className="semfd">TimbuCloud™</p>
+      <p className="semfd">
+        Sign up for our newsletter to receive updates on upcoming products and
+        exclusive discounts on all items
+      </p>
     </div>
   );
 };
